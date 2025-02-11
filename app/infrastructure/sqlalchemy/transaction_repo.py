3bc666b01b0530c4548191774b0b2ecd288a7e4e
Nikita <nikitaivanov2341@gmail.com>
2025-02-11 23:59:45 +0300

    feat: добавлен SQLAlchemy-репозиторий для транзакций
    - Реализован TransactionSQLAlchemyRepository, наследуемый от BaseSQLAlchemyRepository.
    - Добавлен метод get_account_transactions_list для получения списка транзакций по аккаунту.
    - Добавлен метод get_user_transactions_list для получения списка транзакций пользователя.
    - Добавлены методы delete_account_all_transactions и delete_user_all_transactions для удаления всех транзакций по аккаунту и пользователю соответственно.
    - Используется асинхронная сессия SQLAlchemy для выполнения операций с БД.

---
+++ app/infrastructure/sqlalchemy/transaction_repo.py
@@ -0,0 +1,43 @@
+from typing import List, Optional
+from uuid import UUID
+
+from sqlalchemy import delete
+from sqlalchemy.future import select
+
+from domain.transaction import Transaction
+from repositories.transaction_repo import TransactionRepository
+from .base_repo import BaseSQLAlchemyRepository
+
+
+class TransactionSQLAlchemyRepository(BaseSQLAlchemyRepository[Transaction],
+                               TransactionRepository):
+    async def get_account_transactions_list(
+        self,
+        account_id: UUID
+    ) -> List[Transaction]:
+        result = await self._session.execute(
+            select(Transaction).where(Transaction.account_id == account_id)
+        )
+        return result.scalars().all()
+    
+    async def get_user_transactions_list(
+        self,
+        user_id: UUID        
+    ) -> List[Transaction]:
+        result = await self._session.execute(
+            select(Transaction).where(Transaction.user_id == user_id)
+        )
+        return result.scalars().all()
+    
+    async def delete_account_all_transactions(self, account_id):
+        await self._session.execute(
+            delete(Transaction).where(Transaction.account_id == account_id)
+        )
+        await self._session.commit()
+        
+    async def delete_user_all_transactions(self, user_id) -> None:
+        await self._session.execute(
+            delete(Transaction).where(Transaction.user_id == user_id)
+        )
+        await self._session.commit()
+        
