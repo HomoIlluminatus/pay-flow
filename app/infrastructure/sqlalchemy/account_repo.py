6054550e058112fc08a75f542ddae483329c64f0
Nikita <nikitaivanov2341@gmail.com>
2025-02-12 03:39:04 +0300

    fix: в методах класса `AccountSQLAlchemyRepository` заменено `Account` на `self._model`. Убран не используемый импорт.

--- app/infrastructure/sqlalchemy/account_repo.py
+++ app/infrastructure/sqlalchemy/account_repo.py
@@ -2,7 +2,6 @@ from typing import List
 from uuid import UUID
 
 from sqlalchemy import delete
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from domain.account import Account
@@ -14,13 +13,13 @@ class AccountSQLAlchemyRepository(BaseSQLAlchemyRepository[Account],
                                AccountRepository):
     async def get_user_accounts_list(self, user_id: UUID) -> List[Account]:
         result = await self._session.execute(
-            select(Account).where(Account.user_id == user_id)
+            select(self._model).where(self._model.user_id == user_id)
         )
         return result.scalars().all()
     
     async def delete_user_all_accounts(self, user_id) -> None:
         await self._session.execute(
-            delete(Account).where(Account.user_id == user_id)
+            delete(self._model).where(self._model.user_id == user_id)
         )
         await self._session.commit()
         
