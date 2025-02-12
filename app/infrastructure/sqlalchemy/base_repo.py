e0af6c98abc128b14d5d05ccc849fef8a978a7f2
Nikita <nikitaivanov2341@gmail.com>
2025-02-12 03:34:00 +0300

    fix: испарвлен `__init__` в `BaseSQLAlchemyRepository`
    В `__init__` добавлено `model`. `model` - это модель, с которой будет рабтать sqlalchemy.

--- app/infrastructure/sqlalchemy/base_repo.py
+++ app/infrastructure/sqlalchemy/base_repo.py
@@ -10,17 +10,18 @@ from repositories.base_repo import AbstractRepository
 T = TypeVar("T")
 
 class BaseSQLAlchemyRepository(AbstractRepository, Generic[T]):
-    def __init__(self, session: AsyncSession):
+    def __init__(self, session: AsyncSession, model: Generic[T]):
         self._session = session
+        self._model = model
         
     async def get_by_id(self, entity_id: UUID) -> Optional[T]:
             result = await self._session.execute(
-                select(T).where(T.id == entity_id)
+                select(self._model).where(T.id == entity_id)
             )
             return result.scalars().first()
     
     async def get_list(self) -> List[T]:
-        result = await self._session.execute(select(T))
+        result = await self._session.execute(select(self._model))
         return result.scalars().all()
     
     async def add(self, entity: T) -> T:
@@ -34,6 +35,8 @@ class BaseSQLAlchemyRepository(AbstractRepository, Generic[T]):
         return entity
     
     async def delete(self, entity_id: UUID) -> None:
-        await self._session.execute(delete(T).where(T.id == entity_id))
+        await self._session.execute(
+            delete(self._model).where(self._model.id == entity_id)
+        )
         await self._session.commit()
         
